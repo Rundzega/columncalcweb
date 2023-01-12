791988f5103cdf70a6bc7b17aee021ae45d9a52b
rafael <Rundzega>
2023-01-12 16:38:17 -0300

    Modified provisional API IPv4 adress

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: "ec2-3-91-151-191.compute-1.amazonaws.com :8080",
+  baseURL: "http://ec2-3-91-151-191.compute-1.amazonaws.com:8080",
 });
